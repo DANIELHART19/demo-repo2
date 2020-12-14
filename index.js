7de6971cbcd5ca05284253cfaa0f26d4bc1aeaa4
Daniel Hart <daniel.hart19@gmail.com>
2020-12-14 08:29:05 +0000

    project update

---
+++ index.js
@@ -0,0 +1,23 @@
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function () {
+      console.log("draw");
+    },
+  };
+}
+
+const circle = createCircle(1);
+
+circle.draw();
+
+function Circle(radius) {
+  console.log(this);
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw");
+  };
+}
+
+const another = new Circle(1);
+another.draw();
